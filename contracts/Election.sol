ce05541f17a929da868d1955336a126916620f66
Samuel Terra <samuelterra22@hotmail.com>
2019-03-20 22:23:32 -0300

    Add simple Election contract

---
+++ contracts/Election.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.4;
+
+contract Election {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+
+    event votedEvent(
+        uint indexed _candidateId
+    );
+
+    function addCandidate(string _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender]);
+        require(!_candidateId > 0 && _candidateId <= candidatesCount);
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount ++;
+        votedEvent(_candidateId);
+    }
+}
