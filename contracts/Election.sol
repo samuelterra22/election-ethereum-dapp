2668a2d71a4003bddf60c992551a7adfe09a7620
Samuel Terra <samuelterra22@hotmail.com>
2019-03-22 22:03:03 -0300

    Initialize tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,30 +1,27 @@
-pragma solidity ^0.4.4;
+pragma solidity 0.5.6;
 
 contract Election {
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
 
-    mapping(address => bool) public voters;
+    // Store candidates
+    // Fetch Candidate
     mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
     uint public candidatesCount;
 
-    event votedEvent(
-        uint indexed _candidateId
-    );
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
 
-    function addCandidate(string _name) private {
+    function addCandidate(string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    function vote(uint _candidateId) public {
-        require(!voters[msg.sender]);
-        require(!_candidateId > 0 && _candidateId <= candidatesCount);
-        voters[msg.sender] = true;
-        candidates[_candidateId].voteCount ++;
-        votedEvent(_candidateId);
-    }
 }
