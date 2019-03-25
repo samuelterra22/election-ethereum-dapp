c02f1aaa3636c7e85b853eadc192590a4a7e6fbd
Samuel Terra <samuelterra22@hotmail.com>
2019-03-25 09:42:39 -0300

    Add test to check initialized candidates

--- test/election.js
+++ test/election.js
@@ -1,6 +1,7 @@
 const Election = artifacts.require("./Election.sol");
 
 contract('Election', function (accounts) {
+  let electionInstance;
 
   it('initializes with two candidates', function () {
     return Election.deployed().then(function (instance) {
@@ -9,5 +10,21 @@ contract('Election', function (accounts) {
       assert.equal(count, 2)
     });
   });
+
+  it('it initializes the candidates with the correct values', function () {
+    return Election.deployed().then(function (instance) {
+      electionInstance = instance;
+      return electionInstance.candidates(1)
+    }).then(function (candidate) {
+      assert.equal(candidate[0], 1, 'contains the correct id');
+      assert.equal(candidate[1], 'Candidate 1', 'contains the correct name');
+      assert.equal(candidate[2], 0, 'contains the correct vote count')
+      return electionInstance.candidates(2)
+    }).then(function (candidate) {
+      assert.equal(candidate[0], 2, 'contains the correct id');
+      assert.equal(candidate[1], 'Candidate 2', 'contains the correct name');
+      assert.equal(candidate[2], 0, 'contains the correct vote count')
+    })
+  })
 
 });
