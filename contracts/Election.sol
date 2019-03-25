ba9af48d811a9f06ae74f8fd5cda8aca9ecb2b42
Samuel Terra <samuelterra22@hotmail.com>
2019-03-25 15:24:26 -0300

    Implement voted event

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -10,24 +10,28 @@ contract Election {
 
     // Store accounts that have voted
     mapping(address => bool) public voters;
-
-    // Store candidates
+    // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
     // Store Candidates Count
     uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
 
     constructor () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
 
-    function addCandidate(string memory _name) private {
-        candidatesCount++;
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
-    function vote(uint _candidateId) public {
+    function vote (uint _candidateId) public {
         // require that they haven't voted before
         require(!voters[msg.sender]);
 
@@ -37,8 +41,10 @@ contract Election {
         // record that voter has voted
         voters[msg.sender] = true;
 
-        // update candidate vote count
-        candidates[_candidateId].voteCount++;
-    }
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
 
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
 }
